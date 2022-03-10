11bbfa8288be45bb013c735e3904173532f04a0b
asia_demartino@libero.it <asia_demartino@libero.it>
2022-03-10 19:36:41 +0100

    creato componente main

---
+++ src/components/MyMain.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyMain',
+
+}
+</script>
+
+<style lang="scss" scoped>
+.main{
+  height: 100vh;
+  background-color: rgb(23,34,45);
+}
+</style>
